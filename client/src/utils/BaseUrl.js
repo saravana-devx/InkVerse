d5199ee318e3572d87763785789c88a1a9a0a4a3
saravana <118049570+saravana-devx@users.noreply.github.com>
2025-04-27 02:58:38 +0530

    Update BaseUrl.js

--- client/src/utils/BaseUrl.js
+++ client/src/utils/BaseUrl.js
@@ -1,3 +1,9 @@
-const BaseUrl = "https://inkverse-backend-7zc4.onrender.com/api/v1";
+let BaseUrl;
+
+if (process.env.NODE_ENV === 'production') {
+  BaseUrl = `${process.env.REACT_APP_Production_Backend_url}/api/v1`;
+} else {
+  BaseUrl = 'http://localhost:3000/api/v1';
+}
 
 export default BaseUrl;
